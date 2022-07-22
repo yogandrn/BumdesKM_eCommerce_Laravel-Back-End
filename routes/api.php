82c3c53c28bf4ca7f9f2bbd50d43530dbf4c7288
Yoga Andrian <75407016+yogandrn@users.noreply.github.com>
2022-07-22 20:49:55 +0700

    Laravel_API

--- routes/api.php
+++ routes/api.php
@@ -31,27 +31,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UsersController::class, 'login']);
 Route::post('/register', [UsersController::class, 'register']);
-Route::get('/logout', [UsersController::class, 'logout'])->middleware('auth:api');
+Route::get('/logout', [UsersController::class, 'logout']);
 Route::post('/user/check/password', [UsersController::class, 'checkPassword']);
 Route::post('/user/change/password', [UsersController::class, 'changePassword']);
 Route::get('/user/{id}', [UsersController::class, 'getData']);
+Route::post('/user/edit', [UsersController::class, 'editProfile']);
+Route::post('/user/edit/photo', [UsersController::class, 'editPhoto']);
 
 Route::get('/products', [ProductsController::class, 'getAllProduct']);
+Route::get('/products/search/{keyword}', [ProductsController::class, 'searching']);
 Route::get('/products/bestseller', [ProductsController::class, 'getBestSeller']);
-Route::get('/product/show/{id}', [ProductsController::class, 'getDetailProduct']);
+Route::get('/products/newproducts', [ProductsController::class, 'getNewProducts']);
+Route::get('/products/bestseller/all', [ProductsController::class, 'getAllBestSeller']);
+Route::get('/product/show/{id}', [ProductsController::class, 'getDetailProduct'])->name('getProductDetail');
 Route::get('/product/images/{id}', [GalleryController::class, 'getImage']);
 
 Route::get('/carts/user/{id}', [CartController::class, 'getCarts']);
+Route::get('/cart/test', [CartController::class, 'test']);
 Route::delete('/carts/{id}', [CartController::class, 'destroy']);
 Route::post('/add/cart', [CartController::class, 'AddToCart']);
 
+Route::get('/order/test/{id}', [TransactionOutController::class, 'testing']);
+Route::get('/order/test', [TransactionOutController::class, 'test']);
 Route::post('/order', [TransactionOutController::class, 'order']);
+Route::post('/order/rebound', [TransactionOutController::class, 'reboundStock']);
+Route::post('/order/check', [TransactionOutController::class, 'checkOrder']);
 Route::get('/orders/user/{id}', [TransactionOutController::class, 'myorder']);
 Route::get('/order/{id}', [TransactionOutController::class, 'orderInfo']);
 Route::get('/order/{id}/detail', [TransactionDetailController::class, 'detailOrder']);
+Route::post('/order/confirm/{id}', [TransactionOutController::class, 'confirmOrder']);
 
 Route::post('/order/ongkir', [TransactionOutController::class, 'cekOngkir']);
 
 Route::get('/reviews', [ReviewController::class, 'getReviews']);
+Route::post('/review/create', [ReviewController::class, 'createReview']);
 
 Route::post('/payment/upload', [PaymentController::class, 'uploadPayment']);
